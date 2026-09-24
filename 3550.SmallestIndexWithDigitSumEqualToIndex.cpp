1b9b60a1b0e7aaad0fb91c3d97c84de3f50bfbca
Piyush Goyal <168209054+Piyush121Goyal@users.noreply.github.com>
2026-09-24 13:13:07 +0530

    Add solution for 3550. Smallest Index With Digit Sum Equal to Index

---
+++ 3550.SmallestIndexWithDigitSumEqualToIndex.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        for (int i = 0; i < (int)nums.size(); i++) {
+            int x = nums[i], s = 0;
+            while (x > 0) {
+                s += x % 10;
+                x /= 10;
+            }
+            if (s == i) return i;
+        }
+        return -1;
+    }
+};
