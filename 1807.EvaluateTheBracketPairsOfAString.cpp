020ee9f77b2ca18d51343b28b319e0bf4d86086b
Piyush Goyal <168209054+Piyush121Goyal@users.noreply.github.com>
2026-09-26 14:35:51 +0530

    Add solution for 1807. Evaluate the Bracket Pairs of a String

---
+++ 1807.EvaluateTheBracketPairsOfAString.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+        mp.reserve(knowledge.size() * 2);
+        for (auto& kv : knowledge) mp[kv[0]] = kv[1];
+
+        string res;
+        res.reserve(s.size());
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                int j = i + 1;
+                while (s[j] != ')') ++j;
+                string key = s.substr(i + 1, j - i - 1);
+                auto it = mp.find(key);
+                if (it != mp.end()) res += it->second;
+                else res += '?';
+                i = j;
+            } else {
+                res += s[i];
+            }
+        }
+        return res;
+    }
+};
